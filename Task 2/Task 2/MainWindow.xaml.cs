3af746cfb4277e8e9a595cf471b3e2ca8fe23176
garbich <131876643+garbich@users.noreply.github.com>
2024-03-10 13:35:58 +0200

    Add files via upload

---
+++ Task 2/Task 2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            NoteModel noteModel = new NoteModel();
+
+            listBox.Items.Add(noteModel.AddToList(textBox.Text));
+
+               
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            NoteModel noteModel = new NoteModel();
+            
+            object selectedItem = listBox.SelectedItem;
+          
+            noteModel.deleteFromList(listBox, selectedItem);
+            
+        }
+
+ 
+    }
+}
---
+++ Task 2/Task 2/NoteModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Task_2
+{
+    internal class NoteModel
+    {
+        public DateTime CreationDate { get; set; }
+        public string AddToList(string text)
+        {
+          
+           CreationDate = DateTime.Now;
+           return($"Name: {text}                  Time: {CreationDate}");
+        }
+
+        public void deleteFromList(ListBox listBox, object itemToDelete)
+        { 
+             listBox.Items.Remove(itemToDelete);         
+        }
+    }
+}
